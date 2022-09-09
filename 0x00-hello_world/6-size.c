b2850bafd13312fd34f223f499ca916f00b283ef
Dvichi <99565451+Dvichi@users.noreply.github.com>
2022-09-09 05:06:15 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - print in the string in the put fuction
+* main - print the string in the put fuction
 *
 * Description: Using the main function
 * this program prints "the size of various types of computer it is compiled and run on
@@ -8,15 +8,18 @@
 */
 int main(void)
 {
+  
 char c;
 int i;
 long li;
 long long lli;
 float f;
+  
 printf("size of a char: %ld byte(s)\n", sizeof(c));
 printf("size of an int: %ld byte(s)\n", sizeof(i));
 printf("size of a long int: %ld byte(s)\n", sizeof(li));
 printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
 printf("size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
+  
 }
