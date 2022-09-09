15dbc53489f3aaaaaf1614f3a0f77bb420f3ac0a
Dvichi <chbkvctr@gmail.com>
2022-09-08 19:49:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+*Description: using the main function
+* Description: using the main function
+* This program prints "Programming is like building a multingual puzzle"
+* Return: 0
+*/
+int main(void)
+{
+    puts("\""Programming is like building a multingual puzzle");
+    Return (0);
+}
