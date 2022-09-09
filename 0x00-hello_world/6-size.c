0864241957a527028391b5ea8e2aabecf39c994a
Dvichi <chbkvctr@gmail.com>
2022-09-08 20:48:26 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - print the srring in the put fuction
+*
+* Description: a C program that prints the size of various types on the computer it is compiled and run on
+* this program "Prints the size of various types of computer it is compiled and run on
+* Return: 0
+*/
+int main(void)
+{
+char c;
+int i;
+long li;
+long long lli;
+float f;
+
+printf("size of a char: %ld byte(s)\n", size of(c));
+printf("size of an int: %ld byte(s)\n", sizeof(i));
+printf("size of a long int: %ld byte(s)\n", sizeof(li));
+printf("size of a long long int: %id byte(s)\n", sizeof(lli));
+printf("size of a float: %ld byte(s)\n", sizeof(f));
+return (0);
+}
